05f702cbb16681b0ddba6c5dd7224e02df1d32ed
Rk472006 <ramkumar472006@gmail.com>
2025-07-02 03:22:51 +0530

    Delete old backup bclient folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import {  Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Login from "./Auth/Login";
+import Register from "./Auth/Register";
+import Camera from "./Emotion_Detection/Camera";
+import Upload from "./Emotion_Detection/Upload";
+import History from "./History/History";
+import ProtectedRoute from "./Auth/ProtectedRoute";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/camera/:uid" element={<ProtectedRoute><Camera /></ProtectedRoute>} />
+          <Route path="/upload/:uid" element={<ProtectedRoute><Upload /></ProtectedRoute>} />
+          <Route path="/history/:uid" element={<ProtectedRoute><History /></ProtectedRoute>} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
 
-export default App
+        
